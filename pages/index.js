e6f0f94f5419ac9035b29020e2a7b0f0e77b2785
João Mateus Seguessi Munhoz <joao.m.munhoz@gmail.com>
2026-06-22 17:17:26 +0000

    segundo novo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
-    return <h1>Esse é um passo muito importante</h1>
-    <p>Novo Deploy</p>
+    return <h1>Esse é um passo muito importante, novo deploy</h1>
+
 }
 
 export default Home;
