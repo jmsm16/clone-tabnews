95ee7a211571355d5788b7e2bcb454646692bbc8
João Mateus Seguessi Munhoz <joao.m.munhoz@gmail.com>
2026-06-24 14:17:25 +0000

    adiciona scripte

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Esse é um passo muito importante, novo deploy</h1>
-
+function Home() {
+  return <h1>Esse é um passo muito importante, novo deploy</h1>;
 }
 
 export default Home;
